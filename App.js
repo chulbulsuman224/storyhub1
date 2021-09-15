4749ff80d1620961efae2be5a4bda313893bf2ef
chulbulsuman224 <77154652+chulbulsuman224@users.noreply.github.com>
2021-09-15 18:08:52 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,53 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View,Image} from 'react-native';
+import {createAppContainer,createSwitchNavigator} from 'react-navigation'
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import ReadStoryScreen from './Screens/ReadStoryScreen'
+import WriteStoryScreen from './Screens/WriteStoryScreen'
+
+export default function App() {
+  return (
+   <AppContainer/>
+  );
+}
+const TabNavigator=createBottomTabNavigator({
+  ReadStory:{screen:ReadStoryScreen},
+  WriteStory:{screen:WriteStoryScreen}
+},
+{
+  defaultNavigationOptions:({navigation})=>({
+    tabBarIcon:()=>{
+      const routeName=navigation.state.routeName;
+      if(routeName==="ReadStory"){
+        return(
+          <Image
+          source={require("./assets/read.png")}
+          style={{width:40,height:40}}/>
+        )
+      }
+      else if(routeName==="WriteStory"){
+        return(
+          <Image
+          source={require("./assets/write.png")}
+          style={{width:40,height:40}}/>
+        )
+      }
+    }
+  })
+})
+
+const AppContainer=createAppContainer(TabNavigator)
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+
